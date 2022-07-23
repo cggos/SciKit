6e72151392821dcdd37064063c8bf6c71e3a2145
cggos <cggos@outlook.com>
2022-07-23 17:54:39 +0800

    add export_conda_env.sh

---
+++ python/conda_envs/export_conda_env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+param_num=$#
+
+if [ $param_num -ne 1 ]; then
+	echo "Usage: ./export_conda_env.sh <conda-env-name>"
+else
+	env_name="$1"
+	conda env export -n $env_name > env_${env_name}.yml
+fi
