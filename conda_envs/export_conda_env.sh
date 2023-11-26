9b9aa73c8c5cea6246f873da4a5660920d6e9b51
Gavin Gao <cggos@outlook.com>
2023-11-26 23:27:53 +0800

    export doc env with --no-builds

--- conda_envs/export_conda_env.sh
+++ conda_envs/export_conda_env.sh
@@ -6,5 +6,5 @@ if [ $param_num -ne 1 ]; then
 	echo "Usage: ./export_conda_env.sh <conda-env-name>"
 else
 	env_name="$1"
-	conda env export -n $env_name > env_${env_name}.yml
+	conda env export -n $env_name --no-builds > env_${env_name}.yml
 fi
